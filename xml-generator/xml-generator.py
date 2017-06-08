aacc167f87d2fae85cd2a65d7376ca3b29cb5f13
Dizolivemint <dizolivemint@gmail.com>
2017-06-08 10:37:41 -0700

    Print Complete Section 1

--- xml-generator/xml-generator.py
+++ xml-generator/xml-generator.py
@@ -6,36 +6,33 @@ from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 from xml.dom.minidom import parse, parseString
 # https://docs.python.org/3/library/xml.dom.minidom.html
 import datetime
+import operator
 
 def main():
-    """
-    USE FOR CREATING FILE:
+    sectionList, activityList = readmainXML()
 
-    dirList = ['activities','course','files','local','sections']
-    try:
-        directory = raw_input("Please enter a path : ")
-        name = raw_input("Please enter a  filename : ")
-    except:
-        directory = input("Please enter a path : ")
-        name  = input("Please enter a filename : ")
-    try:
-        os.makedirs(directory)
-    except:
-        x = 1
-    filename = directory+ "\ "+name+".xml"
-    num = int(input("How many tags would you like in your xml file : "))
-    a = open(filename,"w")
-    a.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-    for i in range(num):
-       try:
-           tag = raw_input("Please enter a tag name : ")
-       except:
-            tag = input("Please enter a tag name : ")
-       tag_fix = "<"+tag+">"+" "+"</"+tag+">"+"\n"
-       a.write(tag_fix)
-    a.close()
-    return 1
-    """
+    print "\nActivity List"
+    print activityList
+
+    for x in range(0, len(sectionList)):
+        print "\nSection " + sectionList[x][0]
+        print "----------"
+        print "Section Description"
+        print "-------------------"
+        print sectionList[x][1]
+        print "\nActivity Index"
+        print "--------------------"
+        activities = sectionList[x][2].split(',')
+        for activity in activities:
+            print activity
+            try:
+                print activityList.index(activity)
+            except Exception as e:
+                print "None"
+                pass
+
+def moveAct():
+    return 0
 
 def getText(nodelist):
     rc = []
@@ -58,10 +55,13 @@ def sectionParse(sectionPath, directory):
     sectionSummary = getText(domSection.getElementsByTagName("summary")[0].childNodes)
     activitySequence = getText(domSection.getElementsByTagName("sequence")[0].childNodes)
 
+    """
     print "Section #: " + sectionNumber
     print "Section Summary: " + sectionSummary
     print "Sequence of Activities: " + activitySequence
-    return 1
+    """
+
+    return [sectionNumber, sectionSummary, activitySequence]
 
 def assignParse(assignPath, directory):
     directory = os.path.join(directory, 'activities')
@@ -70,11 +70,11 @@ def assignParse(assignPath, directory):
     domAssign = parse(assignPath)
     print "Assignment Path: " + assignPath
     dueDate = getText(domSection.getElementsByTagName("duedate")[0].childNodes)
-    dueDate = datetime.datetime.fromtimestamp(dueDate).strftime('%Y-%m-%dT%H:%M:%SZ')
+    dueDate = datetime.datetime.fromtimestamp(dueDate).strftime('%Y-%m-%dT%H:%M:%S')
     print "Date: " + str(dueDate)
     return 1
 
-def readXML():
+def readmainXML():
     try:
         directory = raw_input("Please enter the full path of moodle_backup.xml \n (e.g. /Users/milesexner/Desktop/Moodle-Course/ws800-01) : ")
     except:
@@ -97,14 +97,18 @@ def readXML():
         modulename = []
         title = []
         location = []
+        activityList = []
         section = []
+        sectionList = []
         for i in range(0, len(moduleid)):
             sectionid.append(dom.getElementsByTagName("sectionid")[i])
+            moduleid.append(dom.getElementsByTagName("moduleid")[i])
             modulename.append(dom.getElementsByTagName("modulename")[i])
             title.append(dom.getElementsByTagName("title")[i])
             location.append(dom.getElementsByTagName("directory")[i])
 
-            section.append(int(showNode(i, sectionid[i], modulename[i], title[i], location[i])))
+            section.append(int(getText(sectionid[i].childNodes)))
+            activityList.append(showNode(i, moduleid[i], sectionid[i], modulename[i], title[i], location[i]))
 
         # section = set(section)
         """
@@ -117,15 +121,37 @@ def readXML():
         print sectionPaths
 
         for x in sectionPaths:
-            sectionParse(x, directory)
+            sectionList.append(sectionParse(x, directory))
             # print x
+            """
+            Each sectionList contains (sectionNumber, sectionSummary, and activitySequence) x number of sections
+            """
 
-        print "\nActivities are location in the following sections: "
+        # sectionList.sort()
+        sectionList = sorted(sectionList,key=lambda x:int(x[0]))
+
+        # sorted(sectionList,key=operator.itemgetter(0))
+        print "\nActivities are located in the following sections: "
         section = list(set(section))
         section.sort()
         print section
 
-        return 1
+        print "\nSections Data"
+        print "-------------"
+        print "Number of Sections"
+        print "(Including General Course Information)"
+        print "--------------------------------------"
+        print len(sectionList)
+        for x in range(0, len(sectionList)):
+            print "\nSection " + sectionList[x][0]
+            print "----------"
+            print "Section Description"
+            print "-------------------"
+            print sectionList[x][1]
+            print "\nActivities by number"
+            print "--------------------"
+            print sectionList[x][2]
+        return sectionList, activityList
 
     """
     def printSections():
@@ -135,24 +161,50 @@ def readXML():
     """
     # printSections()
 
-    def showNode(index, sectionid, modulename, title, location):
-        print ""
-        print "Index: " + str(index)
-        print "Title: " + getText(title.childNodes)
-        print "Section ID: " + getText(sectionid.childNodes)
-        print "Module: " + getText(modulename.childNodes)
-        print "Location: " + getText(location.childNodes)
+    def showNode(index, moduleid, sectionid, modulename, title, location):
+        titleStr = getText(title.childNodes)
+        moduleidStr = getText(moduleid.childNodes)
+        sectionidStr = getText(sectionid.childNodes)
+        modulenameStr = getText(modulename.childNodes)
+        locationStr = getText(location.childNodes)
 
-        modLocation = os.path.join(directory, getText(location.childNodes))
-        modLocation = os.path.join(modLocation, getText(modulename.childNodes).split('_')[0] + ".xml")
+        print "\nIndex: " + str(index)
+        print "Title: " + titleStr
+        print "Module ID: " + moduleidStr
+        print "Section ID: " + sectionidStr
+        print "Module: " + modulenameStr
+        print "Location: " + locationStr
+
+        modLocation = os.path.join(directory, locationStr)
+        modLocation = os.path.join(modLocation, modulenameStr.split('_')[0] + ".xml")
         print "Full Path: " + modLocation
 
-        return getText(sectionid.childNodes)
+        return [moduleidStr, sectionidStr, modulenameStr, titleStr, modLocation]
+        # return 1 # getText(sectionid.childNodes)
 
-    grabObjects(domSource)
-    return 1
+    return grabObjects(domSource)
 
 def writeXML():
+    """
+    USE FOR CREATING FILE:
+    try:
+        os.makedirs(directory)
+    except:
+        x = 1
+    filename = directory+ "\ "+name+".xml"
+    num = int(input("How many tags would you like in your xml file : "))
+    a = open(filename,"w")
+    a.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    for i in range(num):
+       try:
+           tag = raw_input("Please enter a tag name : ")
+       except:
+            tag = input("Please enter a tag name : ")
+       tag_fix = "<"+tag+">"+" "+"</"+tag+">"+"\n"
+       a.write(tag_fix)
+    a.close()
+    return 1
+    """
     return 1
 
-readXML()
+main()
